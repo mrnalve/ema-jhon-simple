58098c1881d7009bdb3a3ce8232aeaf5ec391aee
Md Alve <mdalve1091@gmail.com>
2023-03-28 16:54:30 +0600

    fixed cart added  problem

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -5,16 +5,19 @@ const Cart = ({cart}) => {
     // console.log(cart)
     let totalPrice = 0;
     let totalShipping = 0;
+    let quantity = 0;
     for (const product of cart) {
-       totalPrice += product.price
+        // product.quantity = product.quantity || 1;
+       totalPrice += product.price * product.quantity
        totalShipping += product.shipping
+       quantity += product.quantity
     }
     let tax = totalPrice*7/100
     let grandTotal = totalPrice + totalShipping + tax;
     return (
         <div className='cart'>
             <h5>Order Summary</h5>
-            <p>Selected Item: {cart.length}</p>
+            <p>Selected Item: {quantity}</p>
             <p>Total Price: ${totalPrice}</p>
             <p>Total Shipping: ${totalShipping}</p>
             <p>Tax: {tax.toFixed(2)}</p>
